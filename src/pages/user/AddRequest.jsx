561905010b27276b7a7dcda14ae3396156ea10b2
Anja <136458973+anjaPopovic@users.noreply.github.com>
2025-01-24 17:45:48 +0100

    Update AddRequest.jsx

--- src/pages/user/AddRequest.jsx
+++ src/pages/user/AddRequest.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "../../components/Header";
-import "../../styles/AddRequest.css";
+import {
+    Container,
+    UserRequests,
+    RequestList,
+    RequestItem
+}
+    from "../../styles/Requests";
 
 const AddRequest = () => {
     const [title, setTitle] = useState("");
@@ -9,24 +15,36 @@ const AddRequest = () => {
     const [contentType, setContentType] = useState("Movie");
     const [releaseYear, setReleaseYear] = useState("");
     const [requests, setRequests] = useState([]);
+    const [myRequests, setMyRequests] = useState([]);
 
     const user = localStorage.getItem("username");
 
-    useEffect(() => {
-        const fetchRequests = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5177/requests?user=${user}`);
-                setRequests(response.data);
-            } catch (error) {
-                console.error("Error fetching requests: ", error);
-            }
-        };
+    const fetchRequests = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5177/requests`);
+            const allRequests = response.data;
+            setRequests(allRequests);
 
+            const userRequests = allRequests.filter((req) => req.user === user);
+            setMyRequests(userRequests);
+
+        } catch (error) {
+            console.error("Error fetching requests: ", error);
+        }
+    };
+
+    useEffect(() => {
         fetchRequests();
     }, [user]);
 
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (!title || !country || !releaseYear) {
+            alert("Please fill out all fields!");
+            return;
+        }
 
         const newRequest = {
             title,
@@ -38,8 +56,11 @@ const AddRequest = () => {
         };
 
         try {
-            const nextId = requests.length > 1 ? Math.max(...requests.map(u => u.id)) + 1 : 1;
-            const data = { ...newRequest, id: nextId }; 
+            const nextId = requests.length > 0 ?
+                (Math.max(...requests.map(u => u.id)) + 1).toString()
+                : "1";
+
+            const data = { ...newRequest, id: nextId };
             const response = await axios.post("http://localhost:5177/requests", data);
             alert("Your request has been submitted!");
 
@@ -48,56 +69,64 @@ const AddRequest = () => {
             setContentType("Movie");
             setReleaseYear("");
 
-            setRequests((prevRequests) => [...prevRequests, response.data]);
+            setMyRequests((prevRequests) => [...prevRequests, newRequest]);
+            setRequests((prevRequests) => [...prevRequests, newRequest]);
         } catch (error) {
             console.error("Error submitting request: ", error);
         }
     };
 
     return (
-        <div className="container">
-            <Header isAuthenticated={true} role="user" />
-            <h3>Request a TV Show or Movie</h3>
-            <p>If you have a TV show or a movie you'd like to see on Binge Buddy Application, please fill out this form below! 
-                Do keep in mind that this is the only place to submit content requests.</p>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Title:
-                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </label>
-                <label>
-                    Country:
-                    <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
-                </label>
-                <label>
-                    Type:
-                    <select value={contentType} onChange={(e) => setContentType(e.target.value)}>
-                        <option value="Movie">Movie</option>
-                        <option value="TV Show">TV Series</option>
-                    </select>
-                </label>
-                <label>
-                    Release Year:
-                    <input type="text" value={releaseYear} onChange={(e) => setReleaseYear(e.target.value)} />
-                </label>
-                <button type="submit">Submit Request</button>
-            </form>
+        <>
+            <Header
+                isAuthenticated={true}
+                role="user"
+            />
 
-            <div className="user-requests">
-                <h3>You requested:</h3>
-                {requests.length > 0 ? (
-                    <ul>
-                        {requests.map((req) => (
-                            <li key={req.id}> 
-                                <strong>{req.title}</strong> ({req.releaseYear}) - {req.status}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>You have not made any requests yet.</p>
-                )}
-            </div>
-        </div>
+            <Container>
+                <h3>Request a TV Show or Movie</h3>
+                <p>If you have a TV show or a movie you'd like to see on Binge Buddy Application, please fill out this form below!
+                </p>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Title:
+                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </label>
+                    <label>
+                        Country:
+                        <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
+                    </label>
+                    <label>
+                        Type:
+                        <select value={contentType} onChange={(e) => setContentType(e.target.value)}>
+                            <option value="Movie">Movie</option>
+                            <option value="TV Show">TV Series</option>
+                        </select>
+                    </label>
+                    <label>
+                        Release Year:
+                        <input type="text" value={releaseYear} onChange={(e) => setReleaseYear(e.target.value)} />
+                    </label>
+                    <button type="submit">Submit Request</button>
+                </form>
+
+                <UserRequests>
+                    <h3>You requested:</h3>
+                    {myRequests.length > 0 ? (
+                        <RequestList>
+                            {myRequests.map((req, index) => (
+                                <RequestItem key={index}>
+                                    <strong>{req.title}</strong> ({req.releaseYear}) - {req.status}
+                                </RequestItem>
+                            ))}
+                        </RequestList>
+                    ) : (
+                        <p>You have not made any requests yet.</p>
+                    )}
+                </UserRequests>
+
+            </Container>
+        </>
     );
 };
 
